3c402d60f368caf1b8b8f84d4f1ac953cca2590d
Mark Henderson <mark.henderson1988@gmail.com>
2020-05-31 11:17:38 +0100

    updated to include reader_writer functions

--- azimuth/grid_converter.py
+++ azimuth/grid_converter.py
@@ -1,63 +1,52 @@
-import csv
 import string
 
 from OSGridConverter import grid2latlong
 from pyproj import Proj, transform
+from reader_writer import *
 
 
+# Converts Eastings and Northings to Latitude and Longitude and overwrites .csv file
 def convert_grids_xy(file):
     my_list = []
-    with open(file) as csvfile:
-        reader = csv.reader(csvfile, delimiter=',')
-        for row in reader:
-            in_proj = Proj(init='epsg:27700')
-            out_proj = Proj(init='epsg:4326')
-            x, y = row[0], row[1]
-            x, y = transform(in_proj, out_proj, x, y)
-            lat = y
-            long = x
-            height = row[2]
-            name = row[3]
-            to_write = lat, long, height, name
-            my_list.append(to_write)
-            print(to_write)
-
-        with open('../inputdata/wind_farms.csv', "w", newline='') as csvfile:
-            print("Writing wind_farms.csv: 0%")
-            writer = csv.writer(csvfile)
-            writer.writerows(my_list)
-            print("Writing wind_farms.csv: 100%")
+    reader = read_csv(file)
+    for row in reader:
+        in_proj = Proj(init='epsg:27700')
+        out_proj = Proj(init='epsg:4326')
+        x, y = row[0], row[1]
+        x, y = transform(in_proj, out_proj, x, y)
+        lat, long = y, x
+        height, name = row[2], row[3]
+        to_write = lat, long, height, name
+        my_list.append(to_write)
+        print(to_write)
+        write_csv(file, to_write)
 
 
+# Converts 'Ordinance Survey' grid references into Latitude and Longitude
+# If Lat and long are provided, converts to 4 floating points.
 def convert_grids(file):
     my_list = []
+    # Checks for letters to indicate OS Grid reference
     alpha = string.ascii_letters
 
-    with open(file) as csvfile:
-        reader = csv.reader(csvfile, delimiter=',')
-        for row in reader:
-            if row[0].startswith(tuple(alpha)):
-                location = grid2latlong(row[0])
-                lat = float(location.latitude)
-                long = float(location.longitude)
-                lat = format(lat, '4f')
-                long = format(long, '4f')
-                height = row[1]
-                name = row[2]
-            else:
-                lat = float(row[0])
-                long = float(row[1])
-                lat = format(lat, '4f')
-                long = format(long, '4f')
-                height = row[2]
-                name = row[3]
+    reader = read_csv(file)
+    for row in reader:
+        if row[0].startswith(tuple(alpha)):
+            location = grid2latlong(row[0])
+            lat, long = float(location.latitude), float(location.longitude)
+            lat, long = format(lat, '4f'), format(long, '4f')
+            height, name = row[1], row[2]
+        else:
+            lat, long = float(row[0]), float(row[1])
+            lat, long = format(lat, '4f'), format(long, '4f')
+            height, name = row[2], row[3]
 
-            to_write = lat, long, height, name
-            my_list.append(to_write)
-            print(to_write)
+        to_write = lat, long, height, name
+        my_list.append(to_write)
+        print(to_write)
 
-    with open('../inputdata/wind_farms.csv', "w", newline='') as csvfile:
-        print("Writing wind_farms.csv: 0%")
-        writer = csv.writer(csvfile)
-        writer.writerows(my_list)
-        print("Writing wind_farms.csv: 100%")
+    write_csv(file, to_write)
+
+# For testing
+if __name__ == '__main__':
+    convert_grids_xy('input_data/grid.csv')
