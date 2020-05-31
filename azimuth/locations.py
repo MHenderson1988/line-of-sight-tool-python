2671681489329e82545d601e71da68be165eff49
Mark Henderson <mark.henderson1988@gmail.com>
2020-05-31 11:06:39 +0100

    Added location class and file for common csv read/write functions.

---
+++ azimuth/locations.py
@@ -0,0 +1,30 @@
+class Locations:
+    def __init__(self, a_latitude, a_longitude, a_height, a_name):
+        self.latitude = a_latitude
+        self.longitude = a_longitude
+        self.height = a_height
+        self.name = a_name
+
+    def get_latitude(self):
+        return self.latitude
+
+    def get_longitude(self):
+        return self.longitude
+
+    def get_height(self):
+        return self.height
+
+    def get_name(self):
+        return self.name
+
+    def set_latitude(self, a_latitude):
+        self.latitude = a_latitude
+
+    def set_longitude(self, a_longitude):
+        self.longitude = a_longitude
+
+    def set_height(self, a_height):
+        self.height = a_height
+
+    def set_name(self, a_name):
+        self.name = a_name
