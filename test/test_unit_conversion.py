3839c29f89b660eb3ccb48ad5b64a509ebab8959
Mark Henderson <mark.henderson1988@gmail.com>
2020-07-10 17:18:28 +0100

    Fixed typo in test which was causing a fail

--- test/test_unit_conversion.py
+++ test/test_unit_conversion.py
@@ -5,7 +5,7 @@ from main.unit_conversion import metres_to_feet, nautical_miles_to_statute_miles
 
 class test_unit_conversion(TestCase):
     def test_value_to_convert(self):
-        self.assertEqual(3.28084, metres_to_feet(1))
+        self.assertEqual(3.281, metres_to_feet(1))
 
     def test_nautical_miles_to_statute_miles(self):
         self.assertEqual(1.151, nautical_miles_to_statute_miles(1))
