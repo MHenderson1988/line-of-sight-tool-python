69c113d4614c27f4632182bbaf5d1eeeef415588
Mark Henderson <mark.henderson1988@gmail.com>
2020-07-24 10:23:15 +0100

    Added some exception handling to grid conversion methods

--- main/location_reference_converter.py
+++ main/location_reference_converter.py
@@ -31,15 +31,21 @@ def conversion_type(file, coordinate_type) -> Union[list, int]:
 # object for each position and returns a list of Location objects.  Currently only tests accurate to 6 decimal degrees
 
 def convert_easting_northing(file) -> list:
-    location_list = []
-    with open(file) as csvfile:
-        reader = csv.reader(csvfile, delimiter=',', skipinitialspace='true')
-        for row in reader:
-            transformer = Transformer.from_crs('epsg:27700', 'epsg:4326')
-            x, y = transformer.transform(row[0], row[1])
-            location = Location(round(x, 6), round(y, 6), row[2], row[3])
-            location_list.append(location)
-    return location_list
+    try:
+        location_list = []
+        with open(file) as csvfile:
+            reader = csv.reader(csvfile, delimiter=',', skipinitialspace='true')
+            for row in reader:
+                try:
+                    transformer = Transformer.from_crs('epsg:27700', 'epsg:4326')
+                    x, y = transformer.transform(row[0], row[1])
+                    location = Location(round(x, 6), round(y, 6), row[2], row[3])
+                    location_list.append(location)
+                except Exception:
+                    print("Exceptions at row converting xy to decimal lat/long")
+        return location_list
+    except Exception:
+        print("An error occured converting eastings and northings to decimal lat/long")
 
 
 # This is what the google elevation api accepts.  Therefore a decimal lat long will simply be written to a new
@@ -47,19 +53,22 @@ def convert_easting_northing(file) -> list:
 # Returns a list of Location objects
 
 def convert_decimal_lat_long(file) -> list:
-    location_list = []
-    with open(file) as csvfile:
-        reader = csv.reader(csvfile, delimiter=',', skipinitialspace='true')
-        for row in reader:
-            lat, long = float(row[0]), float(row[1])
-            # Validate the input
-            validate_longitude_latitude(lat, long)
-            height, name = float(row[2]), row[3]
-            # Create a new Locations objects
-            new_location = Location(lat, long, height, name)
-            location_list.append(new_location)
+    try:
+        location_list = []
+        with open(file) as csvfile:
+            reader = csv.reader(csvfile, delimiter=',', skipinitialspace='true')
+            for row in reader:
+                lat, long = float(row[0]), float(row[1])
+                # Validate the input
+                validate_longitude_latitude(lat, long)
+                height, name = float(row[2]), row[3]
+                # Create a new Locations objects
+                new_location = Location(lat, long, height, name)
+                location_list.append(new_location)
 
-    return location_list
+        return location_list
+    except Exception:
+        print("An error occured writing the decimal lat/long")
 
 
 # This function takes an Ordinance Survey National Grid Reference (British National Grid) and converts it to a
@@ -67,14 +76,17 @@ def convert_decimal_lat_long(file) -> list:
 # A list of Location objects is returned.  This currently only passes tests to 4 decimal places / 11.1m.
 
 def convert_british_national_grid(file) -> list:
-    location_list = []
-    with open(file) as csvfile:
-        reader = csv.reader(csvfile, delimiter=',', skipinitialspace='true')
-        for row in reader:
-            grid_converter = OSGridConverter.grid2latlong(row[0])
-            latitude = grid_converter.latitude
-            longitude = grid_converter.longitude
-            height, name = float(row[1]), row[2]
-            new_location = Location(latitude, longitude, height, name)
-            location_list.append(new_location)
-    return location_list
+    try:
+        location_list = []
+        with open(file) as csvfile:
+            reader = csv.reader(csvfile, delimiter=',', skipinitialspace='true')
+            for row in reader:
+                grid_converter = OSGridConverter.grid2latlong(row[0])
+                latitude = grid_converter.latitude
+                longitude = grid_converter.longitude
+                height, name = float(row[1]), row[2]
+                new_location = Location(latitude, longitude, height, name)
+                location_list.append(new_location)
+        return location_list
+    except Exception:
+        print("An error occured converting BNG to decimal latitude/longitude")
