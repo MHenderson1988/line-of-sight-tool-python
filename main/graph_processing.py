eaadb65bc8ac56d59a6365e653c67eabac94eed7
Mark Henderson <mark.henderson1988@gmail.com>
2020-10-27 10:24:21 +0000

    Added functions to show LOS plot as red if view is blocked by terrain.  If LOS exists then plot will be green.  Currently calculated by determining if at the sampled x-coordinate the los plot is of higher or lower value than the terrain y value

--- main/graph_processing.py
+++ main/graph_processing.py
@@ -15,14 +15,17 @@ def create_graph(x_values, y_values, elevation_data, distance, obj_1, obj_2, out
         start_los = elevation_data[0] + float(obj_1.height)
         end_los = elevation_data[-1] + float(obj_2.height)
 
-    # Create a path for the LOS plot
+    # Create a path of y values for the LOS plot
     los_path = create_los_path(x_values, start_los, end_los)
+
+    # Calculate whether or not the LOS Y values intersect with the terrain y values
+    intersection_colour = select_colour(los_path, elevation_data)
 
     base_reg = 0
     plt.figure(figsize=(15, 5))
-    plt.plot(x_values, elevation_data) # Terrain path
-    plt.plot(x_values, y_values) # Earth curvature path
-    plt.plot(x_values, los_path)  # Line of sight path
+    plt.plot(x_values, elevation_data)  # Terrain path
+    plt.plot(x_values, y_values)  # Earth curvature path
+    plt.plot(x_values, los_path, color=intersection_colour)  # Line of sight path
     plt.fill_between(x_values, elevation_data, base_reg, alpha=0.1)
     plt.text(x_values[0], start_los, obj_1.name + ": " + str(obj_1.height))
     plt.text(x_values[-1], end_los, obj_2.name + ": " + str(obj_2.height))
@@ -51,3 +54,35 @@ def create_los_path(x_values, y_start, y_end):
     x = x_values
     y = np.linspace(y_start, y_end, len(x))
     return y
+
+
+"""
+This function compares the two sets of y-values with identical x-values and checks for intersections between the lines.
+If an intersection exists then it returns true, else false. 
+"""
+
+
+def does_intersect(los_y, terrain_y):
+    i = 0
+    intercept = False
+    while i < len(los_y):
+        print(los_y[i], terrain_y[i])
+        if los_y[i] < terrain_y[i]:
+            intercept = True
+            i += 1
+        else:
+            i += 1
+    return intercept
+
+
+"""
+This takes an input of true or false intercept and outputs a colour - red for blocked LOS and green for clear view
+"""
+
+
+def select_colour(los_y, terrain_y):
+    intersection_exists = does_intersect(los_y, terrain_y)
+    colour = 'green'
+    if intersection_exists:
+        colour = 'red'
+    return colour
