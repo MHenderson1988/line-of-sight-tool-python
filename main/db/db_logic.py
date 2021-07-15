bca354833544f0433d7b07e0ddd25ab04d9d1679
Mark Henderson <mark.henderson1988@gmail.com>
2021-07-15 16:03:37 +0100

    more db logic

--- main/db/db_logic.py
+++ main/db/db_logic.py
@@ -1,51 +1,75 @@
 import sqlite3
+from sqlite3 import Error
 
 
 def connect_db():
-    return sqlite3.connect('test.db')
+    conn = None
+    try:
+        conn = sqlite3.connect('test.db')
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+
+    return conn
 
 
-def create_development_table(conn):
-    conn.execute('''
-    CREATE TABLE if not exists developments(
-    development_id INTEGER PRIMARY KEY UNIQUE,
-    name CHAR(50) NOT NULL UNIQUE
-    );
-    ''')
+def create_table(conn, create_table_sql):
+    try:
+        cur = conn.cursor()
+        cur.execute(create_table_sql)
+    except Error as e:
+        print(e)
 
 
-def create_location_table(conn):
-    conn.execute('''
-    CREATE TABLE if not exists location(
-    id INTEGER PRIMARY KEY,
-    development_id INT NOT NULL,
-    name CHAR(50) NOT NULL UNIQUE,
-    x CHAR(20) NOT NULL,
-    y CHAR(20) NOT NULL,
-    height CHAR(10) NOT NULL,
-    
-    FOREIGN KEY(development_id)
-        REFERENCES development(id)
-    );
-    ''')
+def insert_project(conn, project_data):
+    sql = ''' INSERT INTO projects(name) VALUES(?) '''
+    cur = conn.cursor()
+    cur.execute(sql, project_data)
+    conn.commit()
+
+    return cur.lastrowid
 
 
-def insert_to_developments_table(conn, value):
-    curr = conn.cursor()
-    curr.execute('''INSERT INTO developments(name) VALUES (?)''', (value,))
+def insert_location(conn, location_data):
+    sql = ''' INSERT INTO locations(project_id, x, y, height, name) VALUES(?,?,?,?,?)'''
+    cur = conn.cursor()
+    cur.execute(sql, location_data)
+    conn.commit()
+
+    return cur.lastrowid
 
 
 if __name__ == '__main__':
+
+    create_projects_sql = """
+        CREATE TABLE IF NOT EXISTS projects (
+            name TEXT PRIMARY KEY
+        );
+    """
+
+    create_locations_sql = """
+        CREATE TABLE IF NOT EXISTS locations (
+            id INTEGER PRIMARY KEY,
+            project_name TEXT NOT NULL,
+            x TEXT NOT NULL,
+            y TEXT NOT NULL,
+            height TEXT NOT NULL,
+            name TEXT NOT NULL,
+            
+            FOREIGN KEY (project_id) REFERENCES projects(name)
+        );
+    """
+
     conn = connect_db()
-    create_location_table(conn)
-    create_development_table(conn)
-    
-    curr = conn.cursor()
-    curr.execute('''
-    SELECT * FROM developments
-    ''')
 
-    rows = curr.fetchall()
+    if conn is not None:
+        create_table(conn, create_projects_sql)
+        create_table(conn, create_locations_sql)
 
-    for row in rows:
-        print(row)
+        project_1 = ("Sandy Knowe", )
+        location_1 = ("Sandy Knowe", "-4.231212", "56.32312", "149.9", "1")
+
+        insert_location(conn, location_1)
+
+    else:
+        print("Error! Cannot create the database connection")
